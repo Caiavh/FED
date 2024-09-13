24837d05f04a4983e2d1148cb1407733154372b0
Caiavh <152227592+Caiavh@users.noreply.github.com>
2024-09-13 11:41:41 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var receptButton = document.querySelectorAll('.recepten section');
+
+receptButton.addEventListener('click', openRecept);
+
+
+function openRecept() {
+    console.log("hallo fresh");
+    var receptBurger = document.querySelector('article.detailpagina');
+    receptBurger.classList.add('popUp');
+}
